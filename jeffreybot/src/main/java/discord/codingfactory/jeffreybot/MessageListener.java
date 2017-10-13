ef28e196f859585792c9218abc84a0789bf561e3
Alan Drieux <32481840+alan-drieux@users.noreply.github.com>
2017-10-13 14:48:15 +0200

    Add files via upload

---
+++ jeffreybot/src/main/java/discord/codingfactory/jeffreybot/MessageListener.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package discord.codingfactory.jeffreybot;
+
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+/**
+ *
+ * @author alan
+ */
+public class MessageListener extends ListenerAdapter {
+
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+        // We don't want to respond to other bot accounts, including ourself
+        Message message = event.getMessage();
+        String content = message.getRawContent();
+        // getRawContent() is an atomic getter
+        // getContent() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
+        if (content.equals("!ping")) {
+            MessageChannel channel = event.getChannel();
+            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
+        }
+    }
+}
